d7c1643aa6ca74dab22522ff03253629daa3f0fa
enricodelesporte <enricodelesporte@hotmail.com>
2025-08-28 08:23:31 -0300

    Adiciona o campo email ao modelo Paciente e atualiza as classes cadastro e pacienteCRUD para suportar o novo atributo.

--- core/cadastro.py
+++ core/cadastro.py
@@ -2,7 +2,7 @@ from models.pacienteModel import Paciente
 from dataBase import pacienteCRUD
 
 class cadastro:
-    def __init__(self, nome: str, idade: int, CPF: str):
-        paciente = Paciente(nome, idade, CPF)
+    def __init__(self, nome: str, idade: int, CPF: str, email: str):
+        paciente = Paciente(nome, idade, CPF, email)
         pacienteCRUD.adicionar_paciente(paciente)
         return paciente  
--- models/pacienteModel.py
+++ models/pacienteModel.py
@@ -1,8 +1,9 @@
 class Paciente:
-    def __init__(self, nome, idade, CPF):
+    def __init__(self, nome, idade, CPF, email):
         self.nome = nome
         self.idade = idade
         self.CPF = CPF
+        self.email = email
 
     def __str__(self):
-        return f"Nome: {self.nome}, Idade: {self.idade}, CPF: {self.CPF}"
+        return f"Nome: {self.nome}, Idade: {self.idade}, CPF: {self.CPF}, Email: {self.email}"
