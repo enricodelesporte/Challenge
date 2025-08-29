d4332478b859d1374f71fc97e065dac73332db8b
enricodelesporte <enricodelesporte@hotmail.com>
2025-08-29 13:39:59 -0300

    Adiciona métodos de menu para cadastro, consulta, agenda e suporte, melhorando a navegação do usuário.

--- core/menu.py
+++ core/menu.py
@@ -11,4 +11,28 @@ class Menu:
         print("(4) Falar com o suporte.")
         print("(5) Para sair.")
 
-    
+    def menu_cadastro(self):
+        print("----Menu de Cadastro----")
+        print("Qual seu nome:")
+        print("Qual sua idade:")
+        print("Qual seu CPF:")
+        print("(1) Voltar.")
+
+    def menu_consulta(self):
+        print("----Menu de Consulta----")
+        print("Qual o nome do paciente:")
+        print("Qual a especialidade médica:")
+        print("Qual a data da consulta:")
+        print("Qual o horário da consulta:")
+        print("(1) Voltar.")
+
+    def menu_agenda(self):
+        print("----Menu de Agenda----")
+        print("Aqui estão suas consultas agendadas:")
+        print("(1) Voltar.")
+
+    def menu_suporte(self):
+        print("----Menu de Suporte----")
+        print("Como podemos ajudar?")
+        print("(1) Falar com um atendente.")
+        print("(2) Voltar ao menu principal.")
