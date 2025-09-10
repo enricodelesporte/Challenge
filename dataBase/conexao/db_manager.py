dad7d7f63470a5e0b815e18cd5aea22353788da4
enricodelesporte <enricodelesporte@hotmail.com>
2025-09-10 09:12:29 -0300

    Atualização dos CRUDs do Paciente e de Consultas e correção no db_manager

--- dataBase/conexao/db_manager.py
+++ dataBase/conexao/db_manager.py
@@ -1,12 +1,12 @@
 from dataBase.conexao.oracleConexao import oracleConexao
 
 class DBManager:
-    _conexao = None
-    _cursor = None
+    conexao = None
+    cursor = None
 
     @classmethod
     def conectar(cls):
-        if cls._conexao is None or cls._cursor is None:
+        if cls.conexao is None or cls.cursor is None:
             conexao = oracleConexao(
                 user="rm565760",
                 password="150606",
@@ -14,12 +14,12 @@ class DBManager:
                 port="1521",
                 serviceName="orcl"
             )
-            cls._conexao, cls._cursor = conexao.conectar()
-        return cls._conexao, cls._cursor
+            cls.conexao, cls.cursor = conexao.conectar()
+        return cls.conexao, cls.cursor
 
     @classmethod
     def desconectar(cls):
-        if cls._conexao and cls._cursor:
+        if cls.conexao and cls.cursor:
             conexao = oracleConexao("", "", "", "", "")
-            conexao.desconectar(cls._conexao, cls._cursor)
-            cls._conexao, cls._cursor = None, None
+            conexao.desconectar(cls.conexao, cls.cursor)
+            cls.conexao, cls.cursor = None, None
